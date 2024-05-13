51005350a8d5ce0aed3155c7e9a1d834ee0459d6
Michael Guay <michaelguay@Michaels-Mac-Studio.local>
2024-05-12 20:40:59 -0400

    First commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ["dark"],
+  },
 };
 export default config;
